6475989b53827f01cc79a1d4b56d1c407c2d6382
lyxlwz <1850115720@qq.com>
2023-05-22 11:36:06 +0800

    🎉 init

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef, useEffect } from 'react'
+// 导入three.js
+import * as THREE from 'three'
+
 import './App.css'
+import initFun from './hooks/init'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { renderer, render } = initFun(THREE)
+
+  // 初始化dom
+  const containerRef = useRef<HTMLDivElement>(null); // 通过泛型指定 `containerRef` 是一个 `HTMLDivElement`
+  // 挂载完毕后获取dom 
+  useEffect(() => {
+    if (!containerRef.current) {
+      throw new Error("containerRef.current is not valid");
+    }
+    containerRef.current?.appendChild(renderer.domElement)
+
+    // 调用渲染函数
+    render()
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <div> */}
+      {/* 初始化dom */}
+      <div className="container" ref={containerRef}></div>
+      {/* </div> */}
     </>
   )
 }
---
+++ src/hooks/init.ts
@@ -0,0 +1,83 @@
+
+// 导入控制器
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const initFun = (THREE: typeof import("three")) => {
+  // 初始化场景
+  const scene = new THREE.Scene()
+
+  //初始化相机
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    2000
+  )
+  // 设置相机的位置
+  camera.position.set(-50, 50, 130)
+  // 更新摄像头宽高比例
+  camera.aspect = window.innerWidth / window.innerHeight
+  // 更新摄像头投影矩阵
+  camera.updateMatrix()
+  scene.add(camera)
+
+  // 初始化渲染器
+  const renderer = new THREE.WebGLRenderer({
+    // 设置更理想的状态，需要抗锯齿
+    antialias: true
+  })
+  // 设置编码
+  /**
+   * // renderer.outputEncoding = THREE.sRGBEncoding
+   * 
+   * outputEncoding 已废弃 用 outputColorSpace
+   * sRGBEncoding 已废弃 用 SRGBColorSpace
+   */
+  renderer.outputColorSpace = THREE.SRGBColorSpace
+  // 设置渲染器尺寸
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  // 监听屏幕大小改变，修改渲染器的宽高，相机的比例
+  window.addEventListener('resize', () => {
+    // 更新摄像头
+    camera.aspect = window.innerWidth / window.innerHeight
+    // 更新摄像头的头部矩阵
+    camera.updateProjectionMatrix()
+    // 更新渲染器
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  })
+
+  // 将webgl渲染的canvas内容添加到body上
+  document.body.appendChild(renderer.domElement)
+
+  // 设置渲染函数
+  const render = () => {
+    // 渲染场景
+    renderer.render(scene, camera);
+    // 引擎自动更新渲染器
+    requestAnimationFrame(render);
+  }
+  render()
+
+  // 设置物体 -- 球体
+  const geometry = new THREE.PlaneGeometry(100, 100);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+  const plane = new THREE.Mesh(geometry, material);
+  scene.add(plane);
+
+  // 实例化控制器
+  const controls = new OrbitControls(camera, renderer.domElement)
+  // 设置阻尼
+  controls.enableDamping = true
+
+
+
+  return {
+    renderer,
+    render
+  }
+
+}
+
+
+export default initFun
